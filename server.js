6edfbfcfd52aa8af916b0a987643e6e6254342b4
Antigonist <rflynn@brandeis.edu>
2019-08-09 15:22:43 -0400

    breaking liams boilerplate

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
-let express = require('express')
-let app = express()
-let reloadMagic = require('./reload-magic.js')
+let express = require("express");
+let app = express();
+let reloadMagic = require("./reload-magic.js");
 
-reloadMagic(app)
+reloadMagic(app);
 
-app.use('/', express.static('build')); // Needed for the HTML and JS files
-app.use('/', express.static('public')); // Needed for local assets
+app.use("/", express.static("build")); // Needed for the HTML and JS files
+app.use("/", express.static("public")); // Needed for local assets
 
 // Your endpoints go after this line
+app.post("/testing", res => {
+  console.log("this is a test");
+  res.send(JSON.stringify({ success: "none" }));
+});
 
 // Your endpoints go before this line
 
-app.all('/*', (req, res, next) => { // needed for react router
-    res.sendFile(__dirname + '/build/index.html');
-})
+app.all("/*", (req, res, next) => {
+  // needed for react router
+  res.sendFile(__dirname + "/build/index.html");
+});
 
-
-app.listen(4000, '0.0.0.0', () => { console.log("Server running on port 4000") })
+app.listen(4000, "0.0.0.0", () => {
+  console.log("Server running on port 4000");
+});
