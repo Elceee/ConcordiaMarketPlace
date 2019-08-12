748d44140b6da1a5ace4b5b9a36a8a10c343aa7f
Elcee <liamcoughlanlife@gmail.com>
2019-08-12 13:01:42 -0400

    added height to img in Item and text to link

--- src/Item.jsx
+++ src/Item.jsx
@@ -12,12 +12,12 @@ class Item extends Component {
     return (
       <div>
         <h3>{this.props.contents.name}</h3>
-        <img src={this.props.contents.imagePath} />
+        <img src={this.props.contents.imagePath} height="150 px" />
         <div>{this.props.contents.description}</div>
         <div>{this.props.contents.seller}</div>
         <div>Price: ${this.props.contents.price}</div>
         <div>{this.props.contents.stock} in stock</div>
-        <Link to={"/itemdetails/" + this.props.contents._id} />
+        <Link to={"/itemdetails/" + this.props.contents._id}>Item Details</Link>
       </div>
     );
   };
