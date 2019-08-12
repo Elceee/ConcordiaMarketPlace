62aeae0657c0672d78075841971c628562eab2f2
Elcee <liamcoughlanlife@gmail.com>
2019-08-12 13:20:08 -0400

    fixed item details

--- src/ItemDetails.jsx
+++ src/ItemDetails.jsx
@@ -1,35 +1,39 @@
-import React, {Component} from 'react'
-import { Link } from 'react-router-dom'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class ItemDetails extends Component {
-    constructor(props) {
-        super(props) 
-            this.state = {
-                item = ""
-            }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      item: ""
+    };
+  }
 
-    componentDidMount = async () => {
-        let id = this.props._id
-        let data = new FormData()
-        data.append("itemId", id)
-        let response = await fetch("/get-dash-by-id", {method: "POST", body: data})
-        let responseBody = await response.text()
-        let body = JSON.parse(responseBody)
-        if (body.success) {
-            this.setState({item: body.item})
-        }
+  componentDidMount = async () => {
+    let id = this.props._id;
+    let data = new FormData();
+    data.append("itemId", id);
+    let response = await fetch("/get-item-by-id", {
+      method: "POST",
+      body: data
+    });
+    let responseBody = await response.text();
+    let body = JSON.parse(responseBody);
+    if (body.success) {
+      this.setState({ item: body.item });
     }
+  };
 
-
-    render = () => {
-        return (<div>
-            <h3>{this.state.item.name}</h3>
-            <h4>Sold by: {this.state.item.seller}</h4>
-            <div>Sold from {this.location}</div>
-            <div>Reviews here</div>
-        </div>)
-    }
+  render = () => {
+    return (
+      <div>
+        <h3>{this.state.item.name}</h3>
+        <h4>Sold by: {this.state.item.seller}</h4>
+        <div>Sold from {this.location}</div>
+        <div>Reviews here</div>
+      </div>
+    );
+  };
 }
 
-export default ItemDetails
+export default ItemDetails;
