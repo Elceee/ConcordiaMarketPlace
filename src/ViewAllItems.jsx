bb17e654d9ea120ea80fcbd692e08ba5d199fa99
Elcee <liamcoughlanlife@gmail.com>
2019-08-12 11:01:00 -0400

    Added an AllItems Component

---
+++ src/ViewAllItems.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import Item from "./Item.jsxx";
+import { connect } from "react-redux";
+
+class UnconnectedViewAllItems extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount = () => {
+    let getAllItems = async () => {
+      let response = await fetch("/all-items");
+      let responseBody = await response.text();
+      let body = JSON.parse(responseBody);
+      if (body.success !== false) {
+        this.props.dispatch({ type: "updateItems" });
+      }
+    };
+    getAllItems();
+  };
+
+  renderItemsAsLiElems = () => {
+    return (
+      <div>
+        {this.props.items.map(item => {
+          return (
+            <li>
+              <Item contents={item} />
+            </li>
+          );
+        })}
+      </div>
+    );
+  };
+
+  render = () => {
+    return (
+      <div>
+        <ul>{this.renderItemsAsLiElems}</ul>
+      </div>
+    );
+  };
+}
+
+let mapStateToProps = state => {
+  return { items: state.items };
+};
+
+let ViewAllItems = connect(mapStateToProps)(UnconnectedViewAllItems);
+
+export default ViewAllItems;
