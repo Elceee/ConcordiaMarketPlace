220ae7d5753ccd0b4dc8778b127cdd4d59c4d142
Bryce Hamilton <Bryce@macbook.wireless.concordia.ca>
2019-08-12 11:20:47 -0400

    sign up and login

---
+++ src/Signup.jsx
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+class UnconnectedSignup extends Component {
+  constructor(props) {
+    super(props);
+    this.state({ username: "", password: "" });
+  }
+
+  usernameChangeHandler = event => {
+    this.setState({ username: event.target.value });
+  };
+
+  passwordChangeHandler = event => {
+    this.setState({ password: event.target.value });
+  };
+
+  submitHandler = async event => {
+    event.preventDefault();
+    let username = this.state.username;
+    let password = this.state.password;
+    let data = new FormData();
+    data.append("username", username);
+    data.append("password", password);
+    let response = await fetch("/signup", { method: "POST", body: data });
+    let responseBody = await response.text();
+    let parsed = JSON.parse(responseBody);
+    if (!parsed.success) {
+      alert("Username taken");
+      this.setState({ username: "", password: "" });
+      return;
+    }
+    this.props.dispatch({ type: "login-success", username: username });
+  };
+
+  render() {
+    return (
+      <div>
+        <h3>Signup</h3>
+        <form onSubmit={this.submitHandler}>
+          <div>Username</div>
+          <input
+            type="text"
+            onChange={this.usernameChangeHandler}
+            value={this.state.username}
+          />
+          <div>Password</div>
+          <input
+            type="text"
+            onChange={this.passwordChangeHandler}
+            value={this.state.password}
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+let Signup = connect()(UnconnectedSignup);
+
+export default Signup;
