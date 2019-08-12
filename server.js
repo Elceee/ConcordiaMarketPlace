d3b9680f4d61a6a7317b24fb6ed71762afaae8a7
Antigonist <rflynn@brandeis.edu>
2019-08-11 20:42:20 -0400

    added allitems endpoint, multer, and mongo

--- server.js
+++ server.js
@@ -1,16 +1,37 @@
 let express = require("express");
 let app = express();
 let reloadMagic = require("./reload-magic.js");
-
+let multer = require("multer");
+let upload = multer({ dest: __dirname + "/uploads/" });
+app.use("/uploads", express.static("uploads"));
+let dbo = undefined;
+let url = "To be determined"; //We have to add something here.
 reloadMagic(app);
 
 app.use("/", express.static("build")); // Needed for the HTML and JS files
 app.use("/", express.static("public")); // Needed for local assets
+MongoClient.connect(url, { useNewUrlParser: true }, (err, db) => {
+  if (err) {
+    console.log(err);
+  }
+  dbo = db.db("To be determined"); //Let's choose a name to add here.
+});
 
-// Your endpoints go after this line
-app.post("/testing", res => {
-  console.log("this is a test");
-  res.send(JSON.stringify({ success: "none" }));
+// all-itmes endpoint. "items" is my provisional collection name
+app.get("/all-items", (req, res) => {
+  console.log("request to /all-items endpoint");
+  dbo
+    .collection("items")
+    .find({})
+    .toArray((err, items) => {
+      if (err) {
+        console.log("ERROR", err);
+        res.send(JSON.stringify({ success: false }));
+        return;
+      }
+      console.log("items: ", items);
+      res.send(JSON.stringify(items));
+    });
 });
 
 app.get("/delete-all", (req, res) => {
