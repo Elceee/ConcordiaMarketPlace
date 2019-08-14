36b35a118d8cbf80b71fef5435e4b62b102a7d9c
Elcee <liamcoughlanlife@gmail.com>
2019-08-14 11:26:11 -0400

    improved item details, created recinnebded albums

--- src/ItemDetails.jsx
+++ src/ItemDetails.jsx
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import AddReview from "./AddReview.jsx";
+import "./itemDetails.css";
 
 class UnconnectedItemDetails extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      item: ""
+      item: "",
+      allItems: this.props.items
     };
   }
 
@@ -25,6 +27,22 @@ class UnconnectedItemDetails extends Component {
       this.setState({ item: body.item });
     }
   };
+
+  componentDidUpdate = async prevProps => {
+    if (this.props._id !== prevProps._id) {
+      let data = new FormData();
+      data.append("itemId", this.props._id);
+      let response = await fetch("/get-item-by-id", {
+        method: "POST",
+        body: data
+      });
+      let responseBody = await response.text();
+      let body = JSON.parse(responseBody);
+      if (body.success) {
+        this.setState({ item: body.item });
+      }
+    }
+  };
   addToCart = () => {
     let item = this.state.item;
     let data = new FormData();
@@ -33,21 +51,88 @@ class UnconnectedItemDetails extends Component {
     this.props.dispatch({ type: "addToCart", item: item });
   };
 
-  render = () => {
+  findSimilarAlbums = () => {
+    let categories = this.state.item.categories;
+    console.log(categories);
+    let recommendAlbums = [];
+    let allItems = this.state.allItems;
+    categories.forEach(category => {
+      let relatedAlbums = allItems.filter(album => {
+        return album.categories.includes(category);
+      });
+      recommendAlbums = recommendAlbums.concat(relatedAlbums);
+    });
+    return recommendAlbums;
+  };
+
+  threeRandomAlbums = arr => {
+    let currentIndex = arr.length;
+    let tempHold;
+    let ranIndex;
+    if (arr.length <= 1) return arr;
+    while (currentIndex !== 0) {
+      ranIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      tempHold = arr[currentIndex];
+      arr[currentIndex] = arr[ranIndex];
+      arr[ranIndex] = tempHold;
+    }
+    let notSelf = arr.filter(album => {
+      return album._id !== this.state.item._id;
+    });
+    let randomAlbums = notSelf.slice(0, 3);
+    return randomAlbums;
+  };
+
+  renderRecommendedAlbums = () => {
+    let albums = this.threeRandomAlbums(this.findSimilarAlbums());
     return (
       <div>
-        <h3>{this.state.item.name}</h3>
-        <h4>Sold by: {this.state.item.seller}</h4>
-        <div>Sold from </div>
-        <div>Reviews here</div>
+        {albums.map(album => {
+          return (
+            <div>
+              <h3>{album.name}</h3>
+              <img src={album.imagePath} />
+              <div>{album.description}</div>
+              <Link to={"/itemdetails/" + album._id}>Item Details</Link>
+            </div>
+          );
+        })}
+      </div>
+    );
+  };
+
+  render = () => {
+    if (!this.state.item) return "Loading...";
+    return (
+      <div className="itemDetailsContainer">
         <div>
-          <button onClick={this.addToCart}>Add to Cart</button>
+          <div className="card center">
+            <h3>{this.state.item.name}</h3>
+            <img src={this.state.item.imagePath} />
+            <h4>Sold by: {this.state.item.seller}</h4>
+            <div className="price">${this.state.item.price}</div>
+            <div>
+              <button onClick={this.addToCart}>Add to Cart</button>
+            </div>
+          </div>
+          <div className="card center">
+            <h2> People who listen to this ablum also enjoyed!</h2>
+            {this.renderRecommendedAlbums()}
+          </div>
         </div>
-        <AddReview id={this.state.item._id} />
+        <div className="card center">
+          <div>Reviews here</div>
+          <AddReview id={this.state.item._id} />
+        </div>
       </div>
     );
   };
 }
 
-let ItemDetails = connect()(UnconnectedItemDetails);
+let mapStateToProps = state => {
+  return { items: state.items };
+};
+
+let ItemDetails = connect(mapStateToProps)(UnconnectedItemDetails);
 export default ItemDetails;
