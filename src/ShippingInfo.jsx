eff5745fa3026f7e0c2096978fe884dd5322375b
Micah <micahcharlesbrown@gmail.com>
2019-08-19 12:01:51 -0400

    shipping form added

---
+++ src/ShippingInfo.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import "./shippinginfo.css";
+
+class UnconnectedShippingInfo extends Component {
+  constructor(props) {
+    super();
+  }
+  render() {
+    return (
+      <div>
+        <form className="containerShip">
+          <div className="left">
+            Name:
+            <input type="text" value={this.props.username} />
+          </div>
+          <div className="left">
+            Address Line 1:
+            <input className="longBox" type="text" />
+          </div>
+          <div className="left">
+            Address Line 2:
+            <input className="longBox" type="text" />
+          </div>
+          <div className="containerInner">
+            Postal Code:
+            <input type="text" /> City:
+            <input type="text" />
+          </div>
+
+          <div className="containerInner">
+            Province/State:
+            <input type="text" />
+            Country:
+            <input type="text" />
+          </div>
+          <div>
+            Phone Number:
+            <input type="text" />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+let mapStateToProps = state => {
+  return { username: state.username };
+};
+
+let ShippingInfo = connect(mapStateToProps)(UnconnectedShippingInfo);
+
+export default ShippingInfo;
---
+++ src/locations.jsx
@@ -0,0 +1,11 @@
+let locations = {
+  Canada: 5.95,
+  UnitedStates: 6.95,
+  Europe: 25.95,
+  SouthAmerica: 10.95,
+  Africa: 29.95,
+  Asia: 21.95,
+  Australia: 18.99
+};
+
+export default locations;
